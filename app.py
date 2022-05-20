a5a49247099faec5aebec829242e626fcdaf439b
Martin Duong <mdduong@ucdavis.edu>
2022-05-20 04:07:46 -0700

    daily calorie api

--- app.py
+++ app.py
@@ -25,5 +25,31 @@ def calculate_bmi():  # put application's code here
         return "welcome to Bmi calculation page"
 
 
+@app.route('/calculate_daily_calorie', methods=['POST'])
+def calculate_daily_calorie():
+    try:
+        if request.method == 'POST':
+            caloricNeed = 0
+
+            request_data = request.get_json()
+            gender = request_data['genderValue']
+            weight = request_data['weightValue']
+            heightFeet = request_data['heightFeetValue']
+            heightInches = request_data['heightInchesValue']
+            age = request_data['ageValue']
+            activityLevel = request_data['activityLevelValue']
+
+            if(gender == "male"):
+                caloricNeed = (66 + (6.2 * weight) + (12.7 * ((heightFeet * 12) + heightInches)) - (6.76 * age)) * activityLevel
+            elif(gender == "female"):
+                caloricNeed = (655 + (4.35 * weight) + (4.7 * ((heightFeet * 12) + heightInches)) - (4.7 * age)) * activityLevel
+            
+            return jsonify({"caloricNeed": caloricNeed}), 200
+    except Error as e:
+        return {"message": e.message}, 400
+    else:
+        return "Daily Caloric Need API"
+
+
 if __name__ == '__main__':
     app.run()
